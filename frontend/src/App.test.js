dd0e1fa4ff849b34f622563ed13fdbcce8de242d
Pharbie <22204443+Pharbi@users.noreply.github.com>
2025-05-20 22:10:12 -0400

    Update App test to check private mode message

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,45 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+jest.mock('mui-react-hook-form-plus', () => ({
+  HookTextField: () => null,
+  HookSelect: () => null,
+  HookDatePicker: () => null,
+  HookDateTimePicker: () => null,
+  HookTimePicker: () => null,
+  useHookForm: jest.fn(),
+}), { virtual: true });
+
+jest.mock('./components/forms/FormContainer', () => ({
+  WrappedRegistrationForm: () => <div />,
+  WrappedResyTokenForm: () => <div />,
+  WrappedResyResRequestForm: () => <div />,
+  WrappedPasswordResetForm: () => <div />,
+  WrappedRegisterVenueForm: () => <div />,
+  WrappedSignInForm: () => <div />,
+  WrappedBugReportForm: () => <div />,
+}));
+
+jest.mock('./components/user/Profile', () => () => <div />);
+jest.mock('./components/ErrorPage', () => () => <div />);
+jest.mock('./components/user/CheckResyToken', () => () => <div />);
+jest.mock('./components/Navbar', () => () => <div />);
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./firebase', () => ({
+  auth: { onAuthStateChanged: (cb) => { cb(null); return () => {}; } },
+  getResyToken: jest.fn(),
+}));
+
+test('renders private mode message on home page', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  expect(
+    screen.getByText(/Currently in private mode, please login or register for access/i)
+  ).toBeInTheDocument();
 });
